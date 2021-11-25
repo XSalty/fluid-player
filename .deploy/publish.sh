4235e24e72b0a4aede9d4dca24b9a6c5b6f22e27
skall <blazekm1lan@seznam.cz>
2021-11-25 14:24:23 +0100

    Added static player preview

--- .deploy/publish.sh
+++ .deploy/publish.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+cp ./test/preview.html /mnt/img1/home/api/media/xplayer/index.html
+mkdir /mnt/img1/home/api/media/xplayer/static
+cp -r ./test/static/vpaid_linear.xml /mnt/img1/home/api/media/xplayer/static/index.html
+mkdir /mnt/img1/home/api/media/xplayer/src
+cp ./dist/fluidplayer.min.js /mnt/img1/home/api/media/xplayer/src
+find ./dist -name 'hlsjs*.js' -exec cp /mnt/img1/home/api/media/xplayer/src/hlsjs.min.js {} \;
+find ./dist -name 'webvtt*.js' -exec cp /mnt/img1/home/api/media/xplayer/src/webvtt.min.js {} \;
 
 echo "Done"
