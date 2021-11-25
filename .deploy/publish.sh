6e1f23a34c783d81ccc43226b56b017ba7bd4ccf
skall <blazekm1lan@seznam.cz>
2021-11-25 14:29:17 +0100

    Deploy update

--- .deploy/publish.sh
+++ .deploy/publish.sh
@@ -2,10 +2,10 @@
 
 cp ./test/preview.html /mnt/img1/home/api/media/xplayer/index.html
 mkdir /mnt/img1/home/api/media/xplayer/static
-cp -r ./test/static/vpaid_linear.xml /mnt/img1/home/api/media/xplayer/static/index.html
+cp -r ./test/static/vpaid_linear.xml /mnt/img1/home/api/media/xplayer/static/vpaid_linear.xml
 mkdir /mnt/img1/home/api/media/xplayer/src
 cp ./dist/fluidplayer.min.js /mnt/img1/home/api/media/xplayer/src
-find ./dist -name 'hlsjs*.js' -exec cp /mnt/img1/home/api/media/xplayer/src/hlsjs.min.js {} \;
-find ./dist -name 'webvtt*.js' -exec cp /mnt/img1/home/api/media/xplayer/src/webvtt.min.js {} \;
+find ./dist -name '*hlsjs*.js' -exec cp "{}" /mnt/img1/home/api/media/xplayer/src/hlsjs.min.js \;
+find ./dist -name 'webvtt*.js' -exec cp "{}" /mnt/img1/home/api/media/xplayer/src/webvtt.min.js \;
 
 echo "Done"
